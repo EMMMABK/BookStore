124c80e81b11525dd3cc55918747e59b79290b12
EMMMABK <adilbikiev2@gmail.com>
2025-01-25 14:53:33 +0600

    add Fixing Bugs 5-th part

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -6,23 +6,27 @@ const Main = () => {
     const [search, setSearch] = useState("");
     const [bookData,setData] = useState([]);
     const searchBook = (evt) => {
-        if(evt.key === "Enter"){
+        if(evt.key === "Enter" || evt.type === "click"){
             axios.get('https://www.googleapis.com/books/v1/volumes?q='+ search +'&key=YOUR_API' + '&maxResults=40')
             .then(res=>setData(res.data.items))
             .catch(err=>console.log(err))
         } 
     }
+    const handleButtonClick = () => {
+        searchBook({ key: "Enter" }); 
+    };
+
     return(
         <>
             <div className="header">
                 <div className="row_one">
-                    <h1>Book</h1>
+                    <h1>The Reading Room</h1>
                 </div>
                 <div className="row_two">
-                    <h2>Seach your book.</h2>
+                    <h2>Where every page you turn takes you to places you've never been.</h2>
                     <div className="search">
                         <input type="text" placeholder='Enter Your Book Name' value={search} onChange={e => setSearch(e.target.value)} onKeyPress={searchBook}/>
-                        <button><i className="fas fa-search"></i></button>
+                        <button onClick={handleButtonClick}><i className="fas fa-search"></i></button>
                     </div>
                 </div>
             </div>
